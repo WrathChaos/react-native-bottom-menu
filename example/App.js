821b2974131131cf522885d5c0fcc00c054db363
FreakyCoder <kurayogun@gmail.com>
2019-08-05 15:34:40 +0300

    Example and Library is done. README is incoming

--- example/App.js
+++ example/App.js
@@ -1,16 +1,66 @@
-import React, { Fragment } from "react";
-import { StyleSheet, View, StatusBar } from "react-native";
-import BottomMenu from "./lib/src/BottomMenu";
+import React, { Component, Fragment } from "react";
+import { Text, StatusBar, ImageBackground } from "react-native";
+import { BottomMenu, Item } from "react-native-bottom-menu";
+import Androw from "react-native-androw";
+import styles from "./styles/App.style";
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <View style={{ flex: 1, backgroundColor: "#fbfbfb" }}>
-        <BottomMenu />
-      </View>
-    </Fragment>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive: "home" // Initial Active Button
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <ImageBackground
+          style={styles.backgroundStyle}
+          source={require("./assets/Watusi.png")}
+        >
+          <Androw style={styles.headerStyle}>
+            <Text style={styles.textStyle}>React Native Bottom Menu</Text>
+          </Androw>
+          <BottomMenu>
+            <Item
+              size={22}
+              name="home"
+              text="Home"
+              type="Octicons"
+              isActive={this.state.isActive == "home" ? true : false}
+              onPress={() => this.setState({ isActive: "home" })}
+            />
+            <Item
+              size={22}
+              type="Feather"
+              text="Messages"
+              name="message-circle"
+              isActive={this.state.isActive == "messages" ? true : false}
+              onPress={() => this.setState({ isActive: "messages" })}
+            />
+            <Item
+              size={30}
+              name="cart"
+              text="Cart"
+              type="EvilIcons"
+              isActive={this.state.isActive == "cart" ? true : false}
+              onPress={() => this.setState({ isActive: "cart" })}
+            />
+            <Item
+              size={22}
+              name="settings"
+              text="Settings"
+              type="SimpleLineIcons"
+              isActive={this.state.isActive == "settings" ? true : false}
+              onPress={() => this.setState({ isActive: "settings" })}
+            />
+          </BottomMenu>
+        </ImageBackground>
+      </Fragment>
+    );
+  }
+}
 
 export default App;
---
+++ example/styles/App.style.js
@@ -0,0 +1,24 @@
+export default {
+  backgroundStyle: {
+    width: "100%",
+    height: "100%"
+  },
+  headerStyle: {
+    top: "30%",
+    alignSelf: "center",
+    justifyContent: "center",
+    shadowRadius: 3,
+    shadowOpacity: 0.5,
+    shadowColor: "#757575",
+    shadowOffset: {
+      width: 3,
+      height: 3
+    }
+  },
+  textStyle: {
+    fontSize: 45,
+    color: "white",
+    fontWeight: "700",
+    textAlign: "center"
+  }
+};
